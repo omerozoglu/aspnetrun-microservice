4eeaebba385366fbcfa37e45419ccc1cfd9947c8
omerozoglu <omerozoglu.1@gmail.com>
2021-12-18 01:58:15 +0300

    Initial commit

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration confugration)
+        {
+            var client = new MongoClient(confugration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(confugration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(confugration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
